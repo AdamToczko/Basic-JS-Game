1a020e40a61909baf936e9a4e4d45f4f410b926f
Adam Toczko <adamtoczko80@gmail.com>
2019-12-18 10:42:12 +0100

    added ball , testing movement

--- game.js
+++ game.js
@@ -1,11 +1,23 @@
 let canvas;
 let canvasContext;
+let ball = 50;
 
 window.onload = function() {
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
-    this.drawElements();
+
+    let framesPerSecond = 30;
+	setInterval(callBoth, 1000/framesPerSecond);
   
+}
+
+function callBoth() {
+	moveElements();
+	drawElements();
+}
+
+function moveElements() {
+	ball = ball + 5;
 }
 
 function drawElements() {
@@ -15,5 +27,6 @@ function drawElements() {
     canvasContext.fillRect(0,250,20,120);
     canvasContext.fillStyle = "white";
     canvasContext.fillRect(780,250,20,120);
+    canvasContext.fillStyle = 'white';
+	canvasContext.fillRect(ball,100,10,10);
 }
-
